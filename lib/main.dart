cd1502a1aca37c5cf66e7e547766b18a62d447b6
samuel <samuelnoye35@gmail.com>
2024-02-24 02:47:17 +0100

    complete taxi driver app

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taxi_driver/common/color_extension.dart';
+import 'package:taxi_driver/common/db_helper.dart';
+import 'package:taxi_driver/common/globs.dart';
+import 'package:taxi_driver/common/my_http_overrides.dart';
+import 'package:taxi_driver/common/service_call.dart';
+import 'package:taxi_driver/common/socket_manager.dart';
+import 'package:taxi_driver/cubit/login_cubit.dart';
+import 'package:taxi_driver/view/login/splash_view.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+SharedPreferences? prefs;
+
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  DBHelper.shared().db;
+
+  prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp();
+  if (Globs.udValueBool(Globs.userLogin)) {
+    ServiceCall.userObj = Globs.udValue(Globs.userPayload) as Map? ?? {};
+    ServiceCall.userType = ServiceCall.userObj["user_type"] as int? ?? 1;
+  }
+  SocketManager.shared.initSocket();
+
+  runApp(const MyApp());
+  configLoading();
+  ServiceCall.getStaticDateApi();
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..indicatorType = EasyLoadingIndicatorType.ring
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 5.0
+    ..progressColor = TColor.primaryText
+    ..backgroundColor = TColor.primary
+    ..indicatorColor = Colors.white
+    ..textColor = TColor.primaryText
+    ..userInteractions = false
+    ..dismissOnTap = false;
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => LoginCubit())],
+      child: MaterialApp(
+        title: 'Taxi Driver',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: "NunitoSans",
+          scaffoldBackgroundColor: TColor.bg,
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+          ),
+          colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
+          useMaterial3: false,
+        ),
+        home: const SplashView(),
+        builder: EasyLoading.init(),
+      ),
+    );
+  }
+}
---
+++ lib/view/login/splash_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:taxi_driver/common/color_extension.dart';
+import 'package:taxi_driver/common/globs.dart';
+import 'package:taxi_driver/common/service_call.dart';
+import 'package:taxi_driver/view/home/home_view.dart';
+import 'package:taxi_driver/view/login/change_language_view.dart';
+import 'package:taxi_driver/view/login/profile_image_view.dart';
+import 'package:taxi_driver/view/user/user_home_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    super.initState();
+    load();
+   
+  }
+
+  void load() async {
+    await Future.delayed(const Duration(seconds: 3));
+    loadNextScreen();
+  }
+
+  void loadNextScreen() {
+    if (Globs.udValueBool(Globs.userLogin)) {
+      if (ServiceCall.userType == 2) {
+        //Driver Login
+        if (ServiceCall.userObj[KKey.status] == 1) {
+          context.push(const HomeView());
+        } else {
+          context.push(const ProfileImageView());
+        }
+      } else {
+        //User Login
+        context.push(const UserHomeView());
+      }
+    } else {
+      context.push(const ChangeLanguageView());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: TColor.bg,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: context.width,
+            height: context.height,
+            color: TColor.primary,
+          ),
+          Image.asset(
+            "assets/img/app_logo.png",
+            width: context.width * 0.25,
+          )
+        ],
+      ),
+    );
+  }
+}
